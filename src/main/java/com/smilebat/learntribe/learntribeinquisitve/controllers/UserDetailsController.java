4a7ad6dc8073abb13bd975d896296ee35a505679
Pai <sp255138@teradata.com>
2022-09-10 16:50:31 +0530

    Hot Fix for Cors Bean Config

--- src/main/java/com/smilebat/learntribe/learntribeinquisitve/controllers/UserDetailsController.java
+++ src/main/java/com/smilebat/learntribe/learntribeinquisitve/controllers/UserDetailsController.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,6 +31,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequestMapping("/api/v1/profile/user")
+@CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 public class UserDetailsController {
 
